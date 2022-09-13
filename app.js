bfd22fa36a88885e1fa3e9264a333c8692a6c26d
TVinayBabu <109048249+TVinayBabu@users.noreply.github.com>
2022-09-13 21:46:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world by team 4")
+const app = require("express")();
+const PORT = process.env.PORT || 3000;
+app.get("",(req, res) =>{
+    res.send("Hello world by team 4");
+});
+app.listen(PORT, () =>{
+    console.log(`App up at port ${PORT}`);
+});
